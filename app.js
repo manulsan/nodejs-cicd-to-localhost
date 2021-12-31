acfd0638dccc16ca482845f1191b1024d59d11cc
manulsan <alex.damosys@gmail.com>
2021-12-31 16:42:59 +0900

    changed

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path=require('path');
 app.use(express.static(path.join(__dirname,'/frontend/build')))
 app.get('/names',(req,res) => {
     res.send("Telmo");
+    
 })
 
 app.listen(5000,()=> {    
